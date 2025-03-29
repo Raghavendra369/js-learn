d4d748090a7473a7cff039826cd2a169bd40a47b
Raghavendra S B <92967776+Raghavendra369@users.noreply.github.com>
2025-03-29 14:10:04 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accId = 135135
+let accName = "Raghavendra"
+var emailId = "raghu@google.com"
+accCity = "Bangalore"
+// accId = 2 // not allowed to change
+accName = "Raghu"
+emailId = "rgu@rgu.com"
+accCity = "Kalaburagi"
+let accState;
+
+/*
+    never use var in javascript.
+*/
+console.log(accId);
+console.table([accId, accName, emailId, accCity, accState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version.
+
+//alert(3+3) // this is nodejs, not browser.
+
+
+// number
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined 
+// symbol =>unique
+// object
+
+
+console.log(typeof "Raghu");
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
+
