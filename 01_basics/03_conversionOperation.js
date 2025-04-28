65eb4312e9391dc4709ba408e5a0f3bfc9e7a720
Raghavendra S B <92967776+Raghavendra369@users.noreply.github.com>
2025-04-28 14:06:57 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,11 +27,35 @@ console.log(typeof(score));
 
 let islogin = 1
 let booleanislogin = Boolean(islogin)
-console.log(typeof booleanislogin);
-console.log(booleanislogin);
+// console.log(typeof booleanislogin);
+// console.log(booleanislogin);
 
 //  1=>true; 0=>false;
 //""=>false
 //"raghu" =>true
 
 // same can be done from number to string.
+
+
+
+
+//     Operations
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " raghu"
+let str3 =  str1 + str2
+//console.log(str3);
+// console.log("1" + 2);   //12
+// console.log(1 + "2");   //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2");   //32, look ECMAScript.
+
+console.log(true);  //true
+console.log(+true); //1
+
+
+
