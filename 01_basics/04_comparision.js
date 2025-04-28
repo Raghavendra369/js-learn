137ea75c8768f134bc2683a9e29432b39354f5da
Raghavendra S B <92967776+Raghavendra369@users.noreply.github.com>
2025-04-28 14:15:06 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+// console.log("2">1);  // converts the string to number and just checks for the numeral equality.
+// console.log("02" >1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+//  ===
+console.log("2"===2); // it even checks for the datatype equality.
