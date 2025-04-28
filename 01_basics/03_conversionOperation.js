3f987f9dcee88ce6577228ae1e616a8f7746cb32
Raghavendra S B <92967776+Raghavendra369@users.noreply.github.com>
2025-04-28 13:53:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+/*
+let score = 27
+
+console.log(typeof score);
+console.log(typeof(score));
+*/
+
+//let score1 = "27abc"
+//let score1 = null
+//let score1 = undefined
+//let score1 = true
+//let score1 = false
+// let score1 = "Raghu"
+// console.log(typeof score1);
+// console.log(typeof(score1));
+
+// let valueInNumber = Number(score1)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+
+// "27" => 27
+// "27abc" => =>NaN
+// true => 1; false = 0
+
+let islogin = 1
+let booleanislogin = Boolean(islogin)
+console.log(typeof booleanislogin);
+console.log(booleanislogin);
+
+//  1=>true; 0=>false;
+//""=>false
+//"raghu" =>true
+
+// same can be done from number to string.
