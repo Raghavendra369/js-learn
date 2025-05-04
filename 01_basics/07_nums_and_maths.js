945f902a905d0f34ce28fd90eddfff9821542a0f
Raghavendra S B <92967776+Raghavendra369@users.noreply.github.com>
2025-05-04 12:32:56 +0000

    nums and maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance)
+
+
+//console.log(balance.toString().length); // result - 3
+//console.log(balance.toFixed(2));    //result = 100.00
+//console.log(balance.toFixed(1));    // result = 100.0
+const other = 23.8966
+
+// console.log(other.toPrecision(3));  //result = 23.9
+// if other = 123.8966, then console.log(other.toPrecision(3));  results = 124
+
+const hundred = 1000000
+//console.log(hundred.toLocaleString());
+//console.log(hundred.toLocaleString('en-IN'));
